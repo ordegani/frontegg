1c7d3852c97d39db869dcb4d7d55ab23d597bc8e
ordegani <oradeganii@gmail.com>
2022-08-19 23:14:29 +0300

    index.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,11 @@ function App() {
     const baseUrl = ContextHolder.getContext().baseUrl;
     window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
   };
-
+  const showTenant = () => {
+    alert(user
+      .tenantIds);
+  }
+  console.log(user);
   return (
     <div className="App">
       { isAuthenticated ? (
@@ -25,10 +29,26 @@ function App() {
             <span>Logged in as: {user?.name}</span>
           </div>
           <div>
-            <button onClick={() => alert(user.accessToken)}>What is my access token?</button>
+            <label for="user_tenants_dropdown">my tenants: </label>
+            <select name="user_tenants_dropdown">{user.tenants.map((item) => {
+              return (
+                <option>{item.tenantId} 
+      <link href="http://localhost:3000/"></link> 
+                </option>
+              )
+            })}
+            </select>
           </div>
           <div>
-            <button onClick={() => logout()}>Click to logout</button>
+            <button onClick={() => showTenant()}>
+            What is the tenant I'm on right now?
+            </button>
+          </div>
+                    <div>
+                    <button onClick={() => alert(user.accessToken)}>What is my access token?</button>
+                  </div>
+          <div>
+            <button style={{ border: "red solid 2px" }} onClick={() => logout()}>Click to logout</button>
           </div>
           <div><button onClick={handleClick}>Settings</button></div>
         </div>
